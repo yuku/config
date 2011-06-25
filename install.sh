eb6d5ff6e4dd6f7dde101d97b0d9e27aebe8d6a3
Yuku TAKAHASHI <taka84u9@gmail.com>
2011-06-25 23:42:59 +0900

    Make backup when a dotfile already exists.

--- install.sh
+++ install.sh
@@ -1,22 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
+
 function link_file {
     source="${PWD}/$1"
     target="${HOME}/${1/_/.}"
 
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
     ln -sf ${source} ${target}
 }
 
-if [ "$1" = "vim" ]; then
-    for i in _vim*
-    do
-        link_file $i
-    done
-else
-    for i in _*
-    do
-        link_file $i
-    done
-fi
+for i in _*
+do
+    link_file $i
+done
 
 git submodule sync
 git submodule init
