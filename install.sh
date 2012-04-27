9364ffeafadca2da8730daf06a5b4f5f5316f8c9
Yuku Takahashi <taka84u9@gmail.com>
2012-04-28 01:22:37 +0900

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 git submodule sync
 git submodule init
 
+## make symbolic links
 sh update.sh
+
+## install refe
+wget http://doc.okkez.net/archives/201107/ruby-refm-1.9.2-dynamic-20110729.tar.gz
+tar xzvf ruby-refm-1.9.2-dynamic-20110729.tar.gz
+mv ruby-refm-1.9.2-dynamic-20110729 ~/.refe
+ln -s ~/.refe/refe-1_9_2 ~/.refe/refe
 
 ## install vimproc
 unzip src/Shougo-vimproc-ver.5.3-0-g73fa395.zip
