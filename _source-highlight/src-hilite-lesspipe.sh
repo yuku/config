886fdae47a035009ff0f52dc6fc18018c95669d3
Yuku Takahashi <taka84u9@gmail.com>
2012-04-27 14:02:03 +0900

    update

--- _source-highlight/src-hilite-lesspipe.sh
+++ _source-highlight/src-hilite-lesspipe.sh
@@ -2,9 +2,9 @@
 
 for source in "$@"; do
     case $source in
-	*ChangeLog|*changelog) 
+	*ChangeLog|*changelog)
         source-highlight --failsafe -f esc --lang-def=changelog.lang --style-css-file=$HOME/.source-highlight/my.css -i "$source" ;;
-	*Makefile|*makefile) 
+	*Makefile|*makefile)
         source-highlight --failsafe -f esc --lang-def=makefile.lang --style-css-file=$HOME/.source-highlight/my.css -i "$source" ;;
         *) source-highlight --failsafe --infer-lang -f esc --style-css-file=$HOME/.source-highlight/my.css -i "$source" ;;
     esac
