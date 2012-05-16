18a01806f2e1e36e6109ba2a076c6018e7831339
Yuku Takahashi <taka84u9@gmail.com>
2012-05-17 01:29:35 +0900

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# dotfiles
+
 link_file() {
   source="${PWD}/$1"
   target="${HOME}/${1/_/.}"
@@ -18,9 +20,31 @@ for i in _*
 do
   link_file $i
 done
-
 link_file bin
 
 git submodule sync
 git submodule init
 git submodule update
+
+
+# vim
+
+if [ ! -e _vim/bundle/vundle ] ; then
+  vim -c "BundleInstall!"
+fi
+
+cd _vim/bundle/vimproc
+case $OSTYPE in
+darwin*)
+  sofile="autoload/vimproc_mac.so"
+  makefile="make_mac.mak"
+  ;;
+linux*)
+  sofile="autoload/vimproc_unix.so"
+  makefile="make_unix.mak"
+  ;;
+esac
+if [ ! -e $sofile ] ; then
+  make -f $makefile
+fi
+cd $DOTFILES
--- install_refe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wget http://doc.okkez.net/archives/201107/ruby-refm-1.9.2-dynamic-20110729.tar.gz
-tar xzvf ruby-refm-1.9.2-dynamic-20110729.tar.gz
-mv ruby-refm-1.9.2-dynamic-20110729 ~/.refe
-ln -s ~/.refe/refe-1_9_2 ~/.refe/refe
