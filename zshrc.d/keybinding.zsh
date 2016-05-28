00d2365b6b1d7d7033f522b997b11b235213c7c6
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-05-28 13:09:01 +0900

    Add a keybinding to goto project directory

--- zshrc.d/keybinding.zsh
+++ zshrc.d/keybinding.zsh
@@ -5,3 +5,6 @@ bindkey '^G^P' git_pull_current_branch_from_origin
 bindkey '^G^S' git_status
 bindkey '^N'   history-beginning-search-forward-end
 bindkey '^P'   history-beginning-search-backward-end
+
+zle -N peco-src
+bindkey '^[' peco-src
