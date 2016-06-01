b34726433bf66326e323533633a5bd5fbaa9054b
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-06-01 11:58:07 +0900

    Remove unnecessary code

--- zshenv.d/path.zsh
+++ zshenv.d/path.zsh
@@ -5,9 +5,3 @@ path=("$DOTFILES_ROOT/bin" $path)
 if [[ -d "$HOME/bin" ]]; then
     path=("$HOME/bin" $path)
 fi
-
-case ${OSTYPE} in
-darwin*)
-    export PATH="/usr/local/sbin:$PATH"
-    ;;
-esac
