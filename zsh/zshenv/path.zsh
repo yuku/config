8a169433fd8d3740eb78f26b46f89aee1776d9c0
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-06-23 14:17:40 +0900

    Set GOPATH and GOBIN environment variables

--- zsh/zshenv/path.zsh
+++
@@ -1,7 +0,0 @@
-typeset -U path
-
-path=("$DOTFILES_ROOT/bin" $path)
-
-if [[ -d "$HOME/bin" ]]; then
-    path=("$HOME/bin" $path)
-fi
--- zsh/zshenv/variables.zsh
+++ zsh/zshenv/variables.zsh
@@ -1,3 +1,12 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export OUTPUT_CHARSET=utf-8
+export GOPATH=$HOME/.go
+export GOBIN=$GOPATH/bin
+
+typeset -U path
+path=("$DOTFILES_ROOT/bin" $GOBIN $path)
+
+if [[ -d "$HOME/bin" ]]; then
+    path=("$HOME/bin" $path)
+fi
