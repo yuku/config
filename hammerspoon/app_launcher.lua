00a4b9dd9f95e2b379060a5b891d87e774bcd660
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-12-15 16:33:36 +0900

    Add application launcher

---
+++ hammerspoon/app_launcher.lua
@@ -0,0 +1,25 @@
+local app_map = {}
+
+local mash = {"cmd", "ctrl"}
+
+local function registerAppLauncer(modifier, app)
+  table.insert(app_map, string.lower(modifier) .. " - " .. app)
+  hs.hotkey.bind(mash, modifier, function ()
+    hs.application.launchOrFocus(app)
+  end)
+end
+
+registerAppLauncer("I", "iterm")
+registerAppLauncer("C", "Google Chrome")
+
+-- Show help message for application launcher
+hs.hotkey.bind(mash, "/", function ()
+  local function join(delimiter, list)
+    local result = list[1]
+    for i = 2, #list do 
+      result = result .. delimiter .. list[i] 
+    end
+    return result
+  end
+  hs.alert.show(join(", ", app_map), 2)
+end)
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,4 +2,5 @@
 
 hs.window.animationDuration = 0
 
+require("app_launcher")
 require("windows_manipulation")
