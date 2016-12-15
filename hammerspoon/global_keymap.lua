3dd12eccc64576457ed995fbcb8f83eafe8abc82
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-12-15 17:18:04 +0900

    Add global keymapping

---
+++ hammerspoon/global_keymap.lua
@@ -0,0 +1,40 @@
+local function keyCode(key, modifiers)
+  modifiers = modifiers or {}
+  return function()
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+    hs.timer.usleep(1000)
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
+  end
+end
+
+-- Global hotkeys except for iTerm2
+local hotkeys = {}
+table.insert(hotkeys, hs.hotkey.bind({"ctrl"}, "M", keyCode("Return")))
+table.insert(hotkeys, hs.hotkey.bind({"ctrl"}, "H", keyCode("Delete")))
+table.insert(hotkeys, hs.hotkey.bind({"ctrl"}, "N", keyCode("Down")))
+table.insert(hotkeys, hs.hotkey.bind({"ctrl"}, "P", keyCode("Up")))
+
+local function enableHotKeys()
+  for k, v in pairs(hotkeys) do
+    v:enable()
+  end
+end
+
+local function disableHotKeys()
+  for k, v in pairs(hotkeys) do
+    v:disable()
+  end
+end
+
+local function applicationWatcher(name, event, app)
+  if event == hs.application.watcher.activated then
+    if name == "iTerm2" then
+      disableHotKeys()
+    else
+      enableHotKeys()
+    end
+  end
+end
+
+local appWatcher = hs.application.watcher.new(applicationWatcher):start()
+appWatcher:start()
