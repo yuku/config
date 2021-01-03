809cf9bb5680ab478622add69a1eb8f61df5f2a1
Yuku Takahashi <taka84u9@gmail.com>
2021-01-03 10:46:56 +0900

    Refactor eikana.lua

--- dotfiles/.hammerspoon/eikana.lua
+++ dotfiles/.hammerspoon/eikana.lua
@@ -1,34 +1,56 @@
--- init/leftcmd/rightcmd/modified
-local state = "init"
+local Handler = {}
 
-local map = hs.keycodes.map
+function Handler:new()
+  local newObj = {state = "init"}
+  self.__index = self
+  return setmetatable(newObj, self)
+end
 
-local function handleEvent(event)
+function Handler:handle(event)
   local type = event:getType()
-  local keyCode = event:getKeyCode()
-  local flags = event:getFlags()
 
   if type == hs.eventtap.event.types.flagsChanged then
-    if flags["cmd"] then
-      if state == "init" then
-        if keyCode == map["cmd"] then
-          state = "leftcmd"
-        elseif keyCode == map["rightcmd"] then
-          state = "rightcmd"
-        end
-      end
-    else
-      if state == "leftcmd" then
-        hs.keycodes.setLayout("ABC")
-      elseif state == "rightcmd" then
-        hs.keycodes.setMethod("Hiragana")
-      end
-      state = "init"
-    end
+    self:handleFlagsChanged(event)
   elseif type == hs.eventtap.event.types.keyDown then
-    state = "modified"
+    self:handleKeyDown(event)
   end
 end
 
-eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, handleEvent)
+function Handler:isKeyCode(event, code)
+  return event:getKeyCode() == hs.keycodes.map[code]
+end
+
+function Handler:handleFlagsChanged(event)
+  if event:getFlags()["cmd"] then
+    if self.state == "init" then
+      if self:isKeyCode(event, "cmd") then
+        self.state = "leftcmd"
+      elseif self:isKeyCode(event, "rightcmd") then
+        self.state = "rightcmd"
+      end
+    end
+  else
+    if self.state == "leftcmd" then
+      hs.keycodes.setLayout("ABC")
+    elseif self.state == "rightcmd" then
+      hs.keycodes.setMethod("Hiragana")
+    end
+    self.state = "init"
+  end
+end
+
+function Handler:handleKeyDown(event)
+  if self.state == "leftcmd" or self.state == "rightcmd" then
+    self.state = "modified"
+  end
+end
+
+local handler = Handler:new()
+eikana = hs.eventtap.new(
+  {
+    hs.eventtap.event.types.keyDown,
+    hs.eventtap.event.types.flagsChanged
+  },
+  function (event) handler:handle(event) end
+)
 eikana:start()
